ddafc4d5b20f7fc29759c91d271b34465b186cb7
Bruno Gerotto <bedgerotto@gmail.com>
2020-12-02 21:05:50 +0000

    add pwa

---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
+
+
+import GlobalStyles from '../src/styles/global'
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  )
+]
